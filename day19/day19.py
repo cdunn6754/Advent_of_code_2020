cb4d0fa1989363e527b86b7b9bbb05df6b98d4c5
Clint <cdunn6754@gmail.com>
2020-12-29 15:28:47 -0700

    Day 19

--- day19/day19.py
+++ day19/day19.py
@@ -17,8 +17,59 @@ with open(f'{os.getcwd()}/day19/data.txt', 'r') as f:
 # aaaabbb
 # """
 
+# data = """
+# 42: 9 14 | 10 1
+# 9: 14 27 | 1 26
+# 10: 23 14 | 28 1
+# 1: "a"
+# 11: 42 31
+# 5: 1 14 | 15 1
+# 19: 14 1 | 14 14
+# 12: 24 14 | 19 1
+# 16: 15 1 | 14 14
+# 31: 14 17 | 1 13
+# 6: 14 14 | 1 14
+# 2: 1 24 | 14 4
+# 0: 8 11
+# 13: 14 3 | 1 12
+# 15: 1 | 14
+# 17: 14 2 | 1 7
+# 23: 25 1 | 22 14
+# 28: 16 1
+# 4: 1 1
+# 20: 14 14 | 1 15
+# 3: 5 14 | 16 1
+# 27: 1 6 | 14 18
+# 14: "b"
+# 21: 14 1 | 1 14
+# 25: 1 1 | 1 14
+# 22: 14 14
+# 8: 42
+# 26: 14 22 | 1 20
+# 18: 15 15
+# 7: 14 5 | 1 21
+# 24: 14 1
+
+# abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+# bbabbbbaabaabba
+# babbbbaabbbbbabbbbbbaabaaabaaa
+# aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+# bbbbbbbaaaabbbbaaabbabaaa
+# bbbababbbbaaaaaaaabbababaaababaabab
+# ababaaaaaabaaab
+# ababaaaaabbbaba
+# baabbaaaabbaaaababbaababb
+# abbbbabbbbaaaababbbbbbaaaababb
+# aaaaabbaabaaaaababaa
+# aaaabbaaaabbaaa
+# aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+# babaaabbbaaabaababbaabababaaab
+# aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+# """
+
 s_data = data.strip().split('\n\n')
 
+targets = set(s_data[1].split('\n'))
 rule_rhs_pat = re.compile(r'[:,\|] (\d+) ?(\d+)?')
 rule_lhs_pat = re.compile(r'(\d+):')
 rules  = {}
@@ -27,7 +78,7 @@ for rule in s_data[0].split('\n'):
     lhs = int(re.findall(rule_lhs_pat, rule)[0])
     # e.g. rhs = [(72, 10), (5, 112)]
     rhs = re.findall(rule_rhs_pat, rule)
-    # Terminals are letter in this case, they can't be expanded any more
+    # Terminals are letters, they can't be expanded any more
     if len(rhs) == 0:
         terminals[lhs] = re.findall(r'[a-z]', rule)[0]
         continue
@@ -39,7 +90,8 @@ def get_target(rule_num):
     """Given a rule number, find the list of possible terminal strings"""
     if rule_num == '':
         return ['']
-
+    if rule_num in memo:
+        return memo[rule_num]
     rule_num = int(rule_num)
     if rule_num in terminals:
         return terminals[rule_num]
@@ -54,10 +106,51 @@ def get_target(rule_num):
         for t1s in t1_strings:
             for t2s in t2_strings:
                 result_list.append(t1s + t2s)
+
+    memo[rule_num] = result_list
     return result_list
 
-import pdb; pdb.set_trace()
+print(f'Part 1: {len(targets & set(get_target(0)))}')
 
-reachable_targets = set(get_target(0))
-targets = set(s_data[1].split('\n'))
-print(len(targets & reachable_targets))
+# Part 2
+"""
+    Instead of computing all reachable targets, which isn't possible now,
+    just check that all of the provided targets fit a pattern of where at least:
+    1. The string starts with something from S42
+    2. The string ends with something from S31
+    3. The string is composed only of S31 | S42 concatenations
+    4. All S31 strings are after all S42 strings
+    5. x >= y+1 where x is the number of S42 instances and y is the number of S31 instances
+"""
+
+def endswith(s, rule_num):
+    length = len(get_target(rule_num)[0])
+    return s[-length:] in get_target(rule_num)
+
+len_31 = len(get_target(31)[0])
+len_42 = len(get_target(42)[0])
+matching_targets = set()
+for ft in targets:
+    t = ft
+    # There needs to be at least one more instance of 8 chunks in the beginning of the
+    # string as there are 31 chunks at the end, according to the new rules
+    count_31 = 1
+    # Must have at least one string from the 31 set at the end
+    if not endswith(t, 31):
+        continue
+    # Break chunks from the 31 set off the end
+    while endswith(t, 31):
+        count_31 += 1
+        t = t[:-len_31]
+    # Similar to above, it must have at least two strings from the 42 set at the front
+    if len(t) < count_31 * len_42:
+        continue
+    # Break chunks from the 8 set off the end
+    while endswith(t, 42):
+        t = t[:-len_42]
+
+    if len(t) == 0:
+        matching_targets.add(ft)
+
+print(f"Part 2: {len(matching_targets)}")
+
