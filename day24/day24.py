87143934f8e9431349d33acf2d787b1dc5262162
Clint <cdunn6754@gmail.com>
2021-02-02 17:17:43 -0800

    Day 24

--- day24/day24.py
+++ day24/day24.py
@@ -61,5 +61,52 @@ print(f"Part 1: {black_tile_count}")
 
 
 # Part 2
-def neighbor_check(location):
-    """Return the number of white neighbor tiles"""
+def v_add(v1, v2):
+    return (v1[0] + v2[0], v1[1] + v2[1])
+
+def black_check(location):
+    """Return the number of black neighbor tiles"""
+    black_count = 0
+    for vec in dir_map.values():
+        if flip_locations.get(v_add(location, vec), 0) == 1:
+            black_count += 1
+    return black_count
+
+def all_neighbors(location):
+    neighbors = []
+    for vec in dir_map.values():
+        neighbors.append(v_add(location, vec))
+    return neighbors
+
+def step_location(location, color):
+    """Update the location color based on the rules, return the new color"""
+    if color == 0 and black_check(location) == 2:
+        return 1
+    elif color == 1 and black_check(location) == 0 or black_check(location) > 2:
+        return 0
+    return color
+
+def advance(flip_locations):
+    new_locations = {}
+    # Neighbors that have already been investigated
+    dealt_with = set()
+    for location, color in flip_locations.items():
+        new_locations[location] = step_location(location, color)
+
+        # Check neighbors too
+        for n_loc in all_neighbors(location):
+            if n_loc in flip_locations or n_loc in dealt_with:
+                continue
+
+            # Only worry about tiles with exactly two black neighbors
+            # They can only be white at this point
+            if black_check(n_loc) == 2:
+                new_locations[n_loc] = 1
+            dealt_with.add(n_loc)
+    return new_locations
+
+for _ in range(100):
+    flip_locations = advance(flip_locations)
+
+black_tile_count = sum(flip_locations.values())
+print(f"Part 2: {black_tile_count}")
